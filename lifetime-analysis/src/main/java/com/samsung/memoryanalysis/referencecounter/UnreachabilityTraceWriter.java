371dc3c7e8f42bd0551b36e2a64972f841b39516
Manu Sridharan <m.sridharan@samsung.com>
2015-02-27 13:10:01 -0800

    try different output formatting routine

--- lifetime-analysis/src/main/java/com/samsung/memoryanalysis/referencecounter/UnreachabilityTraceWriter.java
+++ lifetime-analysis/src/main/java/com/samsung/memoryanalysis/referencecounter/UnreachabilityTraceWriter.java
@@ -32,34 +32,82 @@ import com.samsung.memoryanalysis.traceparser.TraceAnalysisRunner;
  */
 public class UnreachabilityTraceWriter implements UnreachabilityAwareAnalysis<Void> {
 
-    private final PrintStream out;
+    private final TraceFormatter out;
     private final Gson json;
 
 
     private Timer timer;
     private long counter = 0;
 
-    public UnreachabilityTraceWriter(OutputStream out) {
-        this.out = new PrintStream(out);
-        this.json = new GsonBuilder().create();
-    }
+    private class TraceFormatter {
 
-    @Override
-    public void functionEnter(int iid, int funId, int callSiteIID, Context newContext, long time) {
-        trace(TraceAnalysisRunner.TraceEntry.FUNCTION_ENTER.ordinal(), iid, funId, callSiteIID);
-    }
+        private final PrintStream out;
 
-    private void trace(Object... args) {
-        if (!args[0].equals(TraceAnalysisRunner.TraceEntry.UNREACHABLE.ordinal())) {
-            assert timer.currentTime() == counter;
-            counter++;
+        public TraceFormatter(PrintStream out) {
+            super();
+            this.out = out;
         }
-        out.println(json.toJson(args));
+
+        public TraceFormatter start(int entryType, int iid) {
+            updateCounter(entryType);
+            out.print("[");
+            out.print(entryType);
+            out.print(",");
+            out.print(iid);
+            out.print(",");
+            return this;
+        }
+
+        public TraceFormatter start(int entryType) {
+            updateCounter(entryType);
+            out.print("[");
+            out.print(entryType);
+            out.print(",");
+            return this;
+        }
+
+        public TraceFormatter write(int i) {
+            out.print(i);
+            out.print(",");
+            return this;
+        }
+
+        public void writeEnd(int i) {
+            out.print(i);
+            out.print("]\n");
+        }
+
+        public void writeEnd(long time) {
+            out.print(time);
+            out.print("]\n");
+        }
+
+        public TraceFormatter write(long time) {
+            out.print(time);
+            out.print(",");
+            return this;
+        }
+
+        public TraceFormatter write(String str) {
+            out.print(json.toJson(str));
+            out.print(",");
+            return this;
+        }
+
+        public void close() {
+            out.close();
+
+        }
+
+        public void writeEnd(String str) {
+            out.print(json.toJson(str));
+            out.print("]\n");
+        }
     }
 
-    @Override
-    public void functionExit(int iid, Context functionContext, Set<String> unReferenced, long time) {
-        trace(TraceAnalysisRunner.TraceEntry.FUNCTION_EXIT.ordinal(), iid);
+    public UnreachabilityTraceWriter(OutputStream out) {
+        this.out = new TraceFormatter(new PrintStream(out));
+        this.json = new GsonBuilder().create();
     }
 
     @Override
@@ -67,14 +115,32 @@ public class UnreachabilityTraceWriter implements UnreachabilityAwareAnalysis<Vo
         this.timer = t;
     }
 
+    private void updateCounter(int entryType) {
+        if (entryType != TraceAnalysisRunner.TraceEntry.UNREACHABLE.ordinal()) {
+            assert timer.currentTime() == counter;
+            counter++;
+        }
+    }
+
+    public void functionEnter(int iid, int funId, int callSiteIID, Context newContext, long time) {
+        out.start(TraceAnalysisRunner.TraceEntry.FUNCTION_ENTER.ordinal(), iid).write(funId).writeEnd(callSiteIID);
+    }
+
+
+    @Override
+    public void functionExit(int iid, Context functionContext, Set<String> unReferenced, long time) {
+        out.start(TraceAnalysisRunner.TraceEntry.FUNCTION_EXIT.ordinal()).writeEnd(iid);
+    }
+
+
     @Override
     public void create(int iid, int objectId, long time, boolean isDom) {
-        trace(TraceAnalysisRunner.TraceEntry.CREATE_OBJ.ordinal(), iid, objectId);
+        out.start(TraceAnalysisRunner.TraceEntry.CREATE_OBJ.ordinal(), iid).writeEnd(objectId);
     }
 
     @Override
     public void createFun(int iid, int objectId, int prototypeId, int functionEnterIID, Set<String> namesReferencedByClosures, Context context, long time) {
-        trace(TraceAnalysisRunner.TraceEntry.CREATE_FUN.ordinal(), iid, functionEnterIID, objectId);
+        out.start(TraceAnalysisRunner.TraceEntry.CREATE_FUN.ordinal(), iid).write(functionEnterIID).writeEnd(objectId);
     }
 
     @Override
@@ -82,7 +148,7 @@ public class UnreachabilityTraceWriter implements UnreachabilityAwareAnalysis<Vo
         // we've written out counter elements so far, so the current time should be counter
         // unreachable time shouldn't be after current time
         assert time <= counter;
-        trace(TraceAnalysisRunner.TraceEntry.UNREACHABLE.ordinal(), iid, objectId, time);
+        out.start(TraceAnalysisRunner.TraceEntry.UNREACHABLE.ordinal(), iid).write(objectId).writeEnd(time);
     }
 
     @Override
@@ -92,7 +158,7 @@ public class UnreachabilityTraceWriter implements UnreachabilityAwareAnalysis<Vo
 
     @Override
     public void lastUse(int objectId, int iid, long time) {
-        trace(TraceAnalysisRunner.TraceEntry.LAST_USE.ordinal(), objectId, time, iid);
+        out.start(TraceAnalysisRunner.TraceEntry.LAST_USE.ordinal(), objectId).write(time).writeEnd(iid);
     }
 
     @Override
@@ -103,72 +169,72 @@ public class UnreachabilityTraceWriter implements UnreachabilityAwareAnalysis<Vo
 
     @Override
     public void domRoot(int nodeId) {
-        trace(TraceAnalysisRunner.TraceEntry.DOM_ROOT.ordinal(), nodeId);
+        out.start(TraceAnalysisRunner.TraceEntry.DOM_ROOT.ordinal()).writeEnd(nodeId);
     }
 
     @Override
     public void addDOMChild(int parentId, int childId, long time) {
-        trace(TraceAnalysisRunner.TraceEntry.ADD_DOM_CHILD.ordinal(), parentId, childId);
+        out.start(TraceAnalysisRunner.TraceEntry.ADD_DOM_CHILD.ordinal(), parentId).writeEnd(childId);
     }
 
     @Override
     public void removeDOMChild(int parentId, int childId, long time) {
-        trace(TraceAnalysisRunner.TraceEntry.REMOVE_DOM_CHILD.ordinal(), parentId, childId);
+        out.start(TraceAnalysisRunner.TraceEntry.REMOVE_DOM_CHILD.ordinal(), parentId).writeEnd(childId);
     }
 
     @Override
     public void putField(int iid, int baseId, String offset, int objectId) {
-        trace(TraceAnalysisRunner.TraceEntry.PUTFIELD.ordinal(), iid, baseId, offset, objectId);
+        out.start(TraceAnalysisRunner.TraceEntry.PUTFIELD.ordinal(), iid).write(baseId).write(offset).writeEnd(objectId);
     }
 
     @Override
     public void write(int iid, String name, int objectId) {
-        trace(TraceAnalysisRunner.TraceEntry.WRITE.ordinal(), iid, name, objectId);
+        out.start(TraceAnalysisRunner.TraceEntry.WRITE.ordinal(), iid).write(name).writeEnd(objectId);
     }
 
     @Override
     public void declare(int iid, String name, int objectId) {
-        trace(TraceAnalysisRunner.TraceEntry.DECLARE.ordinal(), iid, name, objectId);
+        out.start(TraceAnalysisRunner.TraceEntry.DECLARE.ordinal(), iid).write(name).writeEnd(objectId);
     }
 
     @Override
     public void updateIID(int objId, int newIID) {
-        trace(TraceAnalysisRunner.TraceEntry.UPDATE_IID.ordinal(), objId, newIID);
+        out.start(TraceAnalysisRunner.TraceEntry.UPDATE_IID.ordinal(),objId).writeEnd(newIID);
     }
 
     @Override
     public void returnStmt(int objId) {
-        trace(TraceAnalysisRunner.TraceEntry.RETURN.ordinal(), objId);
+        out.start(TraceAnalysisRunner.TraceEntry.RETURN.ordinal()).writeEnd(objId);
     }
 
     @Override
     public void debug(int iid, int oid) {
-        trace(TraceAnalysisRunner.TraceEntry.DEBUG.ordinal(), iid, oid);
+        out.start(TraceAnalysisRunner.TraceEntry.DEBUG.ordinal(), iid).writeEnd(oid);
     }
 
 	@Override
 	public void scriptEnter(int iid, String filename) {
-		trace(TraceAnalysisRunner.TraceEntry.SCRIPT_ENTER.ordinal(), iid, filename);
+		out.start(TraceAnalysisRunner.TraceEntry.SCRIPT_ENTER.ordinal(), iid).writeEnd(filename);
 	}
 
 	@Override
 	public void scriptExit(int iid) {
-		trace(TraceAnalysisRunner.TraceEntry.SCRIPT_EXIT.ordinal(), iid);
+		out.start(TraceAnalysisRunner.TraceEntry.SCRIPT_EXIT.ordinal()).writeEnd(iid);
 	}
 
     @Override
     public void topLevelFlush(int iid) {
-        trace(TraceAnalysisRunner.TraceEntry.TOP_LEVEL_FLUSH.ordinal(), iid);
+        out.start(TraceAnalysisRunner.TraceEntry.TOP_LEVEL_FLUSH.ordinal()).writeEnd(iid);
     }
 
     @Override
     public void addToChildSet(int iid, int parentId, String name, int childId) {
-        trace(TraceAnalysisRunner.TraceEntry.ADD_TO_CHILD_SET.ordinal(), iid, parentId, name, childId);
+        out.start(TraceAnalysisRunner.TraceEntry.ADD_TO_CHILD_SET.ordinal(), iid).write(parentId).write(name).writeEnd(childId);
     }
 
     @Override
     public void removeFromChildSet(int iid, int parentId, String name, int childId) {
-        trace(TraceAnalysisRunner.TraceEntry.REMOVE_FROM_CHILD_SET.ordinal(), iid, parentId, name, childId);
+        out.start(TraceAnalysisRunner.TraceEntry.REMOVE_FROM_CHILD_SET.ordinal(), iid).write(parentId).write(name).writeEnd(childId);
     }
 
 
